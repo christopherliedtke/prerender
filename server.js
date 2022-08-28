e9a1ca63667f9f2785b71b82a79c7a947730d25d
Christopher Liedtke <ggwoods@gmx.de>
2022-08-28 07:57:26 +0200

    Added whitelist plugin

--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 const prerender = require("./lib");
 
 const server = prerender({
-  logRequests: true,
+  // logRequests: true,
   chromeLocation: "/app/.apt/usr/bin/google-chrome",
 });
 
 // https://github.com/prerender/prerender-memory-cache
 server.use(require("prerender-memory-cache"));
+
+server.use(require("./lib/plugins/whitelist"));
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
